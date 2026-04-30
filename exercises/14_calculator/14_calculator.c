ba86de543a49de57be852c7a77cde5d7521d0cd6
雨落此间栈 <3473398429@qq.com>
2026-05-01 00:53:27 +0800

    14-2

--- exercises/14_calculator/14_calculator.c
+++ exercises/14_calculator/14_calculator.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>  // 用于处理除零错误时的exit函数
 
 // 定义四则运算规则
-// 处理除数为0的特殊情况
 int add(int a, int b) {
     return a + b;
 }
@@ -18,7 +16,7 @@ int multiply(int a, int b) {
 int divide(int a, int b) {
     if (b == 0) {
         printf("错误：除数不能为零\n");
-        exit(1);  // 终止程序，返回非0状态码
+        return 0;
     }
     return a / b;
 }
@@ -26,15 +24,13 @@ int divide(int a, int b) {
 int main() {
     int a, b;
     char op;
-
-    // 定义函数指针数组，元素类型为int (*)(int, int)
+    // 定义函数指针数组
     int (*operations[])(int, int) = {add, subtract, multiply, divide};
-
     char operators[] = "+-*/";
-
+    
     printf("输入两个整数和一个运算符 (+, -, *, /): ");
     scanf("%d %d %c", &a, &b, &op);
-
+    
     // 查找对应的运算符索引
     int index = -1;
     for (int i = 0; i < 4; i++) {
@@ -43,14 +39,13 @@ int main() {
             break;
         }
     }
-
+    
     if (index == -1) {
         printf("无效的运算符\n");
         return 1;
     }
-
+    
     int result = operations[index](a, b);
     printf("%d %c %d = %d\n", a, op, b, result);
-
     return 0;
 }
